1f3266ed5cd17a17bf7d8f03234e77561f7de77c
garve32 <garve32@gmail.com>
2022-07-20 20:32:50 +0900

    [QUIZ-13] /api 로 호출하는 요청은 allow origin 허용

--- src/main/java/com/ict/quiz/config/WebConfig.java
+++ src/main/java/com/ict/quiz/config/WebConfig.java
@@ -23,8 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*");
+        registry.addMapping("/api/**").allowedOrigins("*");
         WebMvcConfigurer.super.addCorsMappings(registry);
     }
 
